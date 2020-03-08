1a68ce1a1b519aa91fd07f11d2a66e96f2e6b28e
Sam van Schaik <development.siriso@gmail.com>
2020-03-08 02:32:48 +0100

    .

--- javascript/transition.js
+++ javascript/transition.js
@@ -1,53 +1,37 @@
-function fadeOut() {
-    console.log('fade out');
-    var x = document.getElementById("title-container");
-    var y = document.getElementById("portfolio-container");
-
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
-            x.style.display = 'none';
-        }
-        x.style.opacity = op;
-        x.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
-
-    setTimeout(function() {
-        this.fadeIn();
-    }, 1000)
-
-}
-
-function fadeIn() {
-    var x = document.getElementById("portfolio-container");
-    var op = 0.1;  // initial opacity
-    x.style.display = 'block';
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        x.style.opacity = op;
-        x.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 50);
-}
 
 function changeContent(displayedContent) {
+    let content = document.getElementById('content-container').children;
     let mainContainer = document.getElementById('main-container');
     let portfolioContainer = document.getElementById('portfolio-container');
     let servicesContainer = document.getElementById('services-container');
+    let servicesItemContainer = document.getElementById('services-item-container');
     let aboutmeContainer = document.getElementById('aboutme-container');
 
-    mainContainer.style.opacity = '0';
-    portfolioContainer.style.opacity = '0';
-    servicesContainer.style.opacity = '0';
-    aboutmeContainer.style.opacity = '0';
+    // mainContainer.style.opacity = '0';
+    // portfolioContainer.style.opacity = '0';
+    // servicesContainer.style.opacity = '0';
+    // aboutmeContainer.style.opacity = '0';
 
+    for(let i=0; i<content.length; i++){
+        content[i].style.opacity = '0';
+    }
+
+    servicesContainer.style.left = '50%';
+    document.getElementById('webdevelopment-container').style.display = 'none';
 
    setTimeout(function () {
        document.getElementById(displayedContent).style.opacity = '1';
    }, 500)
 };
 
+function transitionServices(displayedService) {
+    let servicesContainer = document.getElementById('services-container');
+
+    servicesContainer.style.left = '10%';
+
+    document.getElementById('webdevelopment-container').style.display = 'block';
+
+    setTimeout(function () {
+        document.getElementById(displayedService).style.opacity = '1';
+    }, 500)
+}
